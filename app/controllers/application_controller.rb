f9f87248bdbd9e274ab2c3f7d59a41a576dd6296
steveobrian <steve.krasel@posteo.de>
2018-10-16 14:51:48 +0200

    Uses basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
