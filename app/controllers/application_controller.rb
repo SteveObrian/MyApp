366bfe811773c17caa561ee8329078a249c63ce5
steveobrian <steve.krasel@posteo.de>
2018-10-16 15:06:42 +0200

    Uses basic auth only on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
