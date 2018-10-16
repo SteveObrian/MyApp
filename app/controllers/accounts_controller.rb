2be98f6240dfa70e1ecb7e7b58e4c940323d3803
steveobrian <steve.krasel@posteo.de>
2018-10-16 16:57:50 +0200

    Removes obsolete pieData variables

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,50 +2,11 @@ class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :edit, :update, :destroy]
 
   def index
-    @accounts = Account.all
+    @accounts = Account.where(user_id: current_user.id)
   end
 
   def show
     @transactions = @account.transactions.all
-
-    @pieSize = {
-      :height => 500,
-      :width => 500
-    }
-
-    @pieData = [
-          {
-            value: 300,
-            color:"#F7464A",
-            highlight: "#FF5A5E",
-            label: "Red"
-          },
-          {
-            value: 50,
-            color: "#46BFBD",
-            highlight: "#5AD3D1",
-            label: "Green"
-          },
-          {
-            value: 100,
-            color: "#FDB45C",
-            highlight: "#FFC870",
-            label: "Yellow"
-          },
-          {
-            value: 40,
-            color: "#949FB1",
-            highlight: "#A8B3C5",
-            label: "Grey"
-          },
-          {
-            value: 120,
-            color: "#4D5360",
-            highlight: "#616774",
-            label: "Dark Grey"
-          }
-
-        ].to_json
   end
 
   def new
